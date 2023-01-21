c376236a9a2eb7edc8ba073ee47fdc43582443d6
Philip <philipwinduong@gmail.com>
2023-01-21 13:15:17 -0800

    Update Champion model to account for loading_screen_url

--- src/main/java/higherlowerlolchamp/backend/model/Champion.java
+++ src/main/java/higherlowerlolchamp/backend/model/Champion.java
@@ -3,6 +3,7 @@ package higherlowerlolchamp.backend.model;
 public class Champion {
     private String championName;
     private String matchesPlayed;
+    private String loadingScreenUrl;
 
 
     public Champion() {}
@@ -20,6 +21,10 @@ public class Champion {
         return matchesPlayed;
     }
 
+    public String getLoadingScreenUrl() {
+        return loadingScreenUrl;
+    }
+
     public void setChampionName(String championName) {
         this.championName = championName;
     }
@@ -28,9 +33,13 @@ public class Champion {
         this.matchesPlayed = matchesPlayed;
     }
 
+    public void setLoadingScreenUrl(String loadingScreenUrl) {
+        this.loadingScreenUrl = loadingScreenUrl;
+    }
+
     @Override
     public String toString() {
-        return String.format("Champion[championName='%s', matchesPlayed='%s']",
-        championName, matchesPlayed);
+        return String.format("Champion[championName='%s', matchesPlayed='%s', loadingScreenUrl='%s']",
+        championName, matchesPlayed, loadingScreenUrl);
     }
 }
