55c7ea84e43a14af6fa46788fad56188b4deed29
Philip <philipwinduong@gmail.com>
2023-01-22 14:29:10 -0800

    Add CORS for localhost:3000

--- src/main/java/higherlowerlolchamp/backend/BackendApplication.java
+++ src/main/java/higherlowerlolchamp/backend/BackendApplication.java
@@ -2,12 +2,24 @@ package higherlowerlolchamp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/champions").allowedOrigins("http://localhost:3000");
+			}
+        };
+    }
 }
